72b4a6a9ff659b85942e24a8c97437fbc59b734d
Ivan Arriola <74118394+Autodidacta-TI@users.noreply.github.com>
2021-12-06 20:21:56 -0300

    ADD Hide Logo Login

---
+++ ati_hide_logo_login/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2020-Present Autodidacta TI. (<https://autodidactati.com>)
+
+{
+    'name': 'Hide Logo Login',
+    "version": "13.0",
+    'author': 'Ivan Arriola - Autodidacta TI',
+    'category': 'Extra Tools',
+    'license': 'OPL-1',
+    'website': 'https://autodidactati.com',
+    'summary': 'Oculta Logo principal de Login',
+    'description': '''Oculta Logo principal de Login''',
+    'depends': ['web'],
+    'data': [
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
